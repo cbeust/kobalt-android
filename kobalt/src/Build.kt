ae7457fa25da6c1cdece01f3b1d883c5b24b054e
Cedric Beust <cedric@beust.com>
2016-03-05 00:40:10 +0400

    0.46.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -20,7 +20,7 @@ val p = project {
     name = "kobalt-android"
     artifactId = name
     group = "com.beust"
-    version = "0.44"
+    version = "0.46"
 
     dependencies {
         compile("com.android.tools.build:builder:2.0.0-alpha3")
